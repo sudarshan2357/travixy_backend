7b394683174613d2c8b4d0071fcf66ad93134793
Rishabh singh <rishabhsingh2537@gmail.com>
2024-11-04 18:01:04 +0530

    Initial commit

---
+++ controllers/userController.js
@@ -0,0 +1,116 @@
+import User from "../models/User.js";
+
+// create new User 
+export const createUser = async (req, res) => {
+
+    const newUser = new User(req.body);
+    try {
+        const savedUser = await newUser.save();
+        res
+            .status(200)
+            .json({
+                success: true,
+                message: "User created successfully",
+                data: savedUser,
+            });
+    } catch (err) {
+        console.error("Error saving User:", err);
+        res
+            .status(500)
+            .json({
+                success: false,
+                message: "Failed!! Try again",
+        });
+    }
+};
+// update User
+export const updateUser = async (req, res) => {
+
+    const id = req.params.id;
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            id,
+            {
+                $set: req.body,
+            },
+            { new: true }
+        );
+        res
+        .status(200)
+        .json({
+            success: true,
+            message: "User updated successfully",
+            data: updatedUser,
+        });
+    } catch (err) {
+        res
+        .status(500)
+        .json({
+            success: false,
+            message: "Failed to update",
+        });
+    }
+};
+// delete User
+export const deleteUser = async (req, res) => {
+    const id = req.params.id;
+    try {
+        await User.findByIdAndDelete(id);
+        res
+        .status(200)
+        .json({
+            success: true,
+            message: "Deleted successfully",
+        });
+    } catch (err) {
+        res
+        .status(500)
+        .json({
+            success: false,
+            message: "Failed to delete",
+        });
+    }
+};
+// getSingle User
+export const getSingleUser = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const user = await User.findById(id);
+        res
+        .status(200)
+        .json({
+            success: true,
+            message: "Fetched successfully",
+            data: user
+        });
+    } catch (err) {
+        res
+        .status(404)
+        .json({
+            success: false,
+            message: "not found",
+        });
+    }
+};
+// getAll User
+export const getAllUser = async (req, res) => {
+
+    try {
+
+        const users = await User.find({})
+        res
+        .status(200)
+        .json({
+            success: true,
+            message: "Successful",
+            data: users,
+        });
+    } catch (err) {
+        res
+        .status(404)
+        .json({
+            success: false,
+            message: "not found",
+        });
+    }
+};
---
+++ models/Tour.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const tourSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        destinations: {
+            type: String,   
+            required: true,
+        },
+        distance: {
+            type: Number,
+            required: true,
+        },
+        photo: {
+            type: String,
+            required: true,
+        },
+        maxGroupSize: {
+            type: Number,
+            required: true,
+        },
+        desc: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+
+        reviews: [
+            {
+                type: mongoose.Types.ObjectId,
+                ref: "Review",
+            }
+        ],
+        featured: {
+            type: Boolean,
+            default: false,
+        },
+    },  
+    { timestamps: true }
+);  
+
+export default mongoose.model("Tour", tourSchema);
